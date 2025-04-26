9b950ee5c305a5ddea55ea4843bff54c9090ec6f
Jake Kealey <jake.kealey@nhs.net>
2025-04-26 11:35:17 +0100

    Index file processing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
+import numpy as np
+import pandas as pd
+
+from datetime import datetime as dt
+from sqlalchemy import create_engine, MetaData, Table, text, insert
+
+from os import listdir
+from os.path import isfile, join
+
+import utils.database_util as db
 import utils.scrape_util as scrape
 
 #Scrape and download the latest data files from the NHSD site
@@ -54,5 +64,123 @@ def scrape_latest_data(
             file_name = file_id + ".xlsx"
 
             scrape.save_file(content, file_name)
-                    
-scrape_latest_data()
+
+#Upload the survival data
+def upload_survival_data(data, table, dsn="SANDPIT", 
+                         database="Data_Lab_NCL_Dev", 
+                         schema="GrahamR"):
+    
+    #Establish connection
+    engine = db.db_connect(dsn, database)
+
+    with engine.connect() as con:
+        #Truncate existing data to prevent overlapping data in the table
+        con.execute(text(f"TRUNCATE TABLE [{schema}].[{table}];"))
+
+        #Upload the data
+        data.to_sql(table, engine, schema=schema, if_exists="append", 
+                    index=False, chunksize=100, method="multi")
+        
+        con.commit()
+
+#Function for processing the index data
+def process_index_data(data_file, target_geographies):
+
+    #Extract data###############################################################
+
+    df_index = pd.read_excel(data_file, sheet_name="Table 5", skiprows=10)
+
+    #Transform data#############################################################
+
+    #Filter to select geographies
+    df_index = df_index[df_index["Geography code"].isin(target_geographies)]
+
+    #Derive data_substituion
+    df_index["data_substitued"] = np.where(
+        df_index["Substituted by Other Geography"].isnull() == 8, 0, 1)
+
+    #Stamp data with timestamp
+    df_index["date_uploaded"] = dt.today()
+
+    #Populate extra Persons rows
+    df_breast_persons = df_index[(df_index["Cancer site"] == "Breast") 
+                                 & (df_index["Gender"] == "Female")].copy()
+    df_breast_persons["Gender"] = "Persons"
+    df_index = pd.concat([df_index, df_breast_persons])
+
+    #Remove Unused Columns
+    columns_to_keep = [
+        "Geography name",
+        "Geography code",
+        "Cancer site", 
+        "Gender",
+        "Age at diagnosis",
+        "Standardisation type",
+        "Diagnosis year",
+        "Years since diagnosis",
+        "Patient numbers",
+        "Survival (%)",
+        "Lower CI",
+        "Upper CI",
+        "Precision",
+        "Standard error",
+        "data_substitued",
+        "date_uploaded"]
+    
+    df_index = df_index[columns_to_keep]
+
+    #Rename columns
+    column_map = {
+        "Geography name": "Area name",
+        "Area code": "Area code",
+        "Survival (%)": "sruvival_per"
+    }
+
+    df_index = df_index.rename(column_map)
+
+    #Format column names
+    df_index.columns = df_index.columns.str.replace('\n', ' ', regex=False)
+    df_index.columns = df_index.columns.str.strip().str.replace(' ', '_')
+    df_index.columns = df_index.columns.str.lower()
+
+    print(df_index.shape)
+
+    #Load data##################################################################
+    upload_survival_data(df_index, table="cancer_survival_index")
+
+
+#Function for processing the adult cancer survival (Table 4) data
+def process_adult_data_sheet4(data_file, target_geographies):
+    pass
+
+def main(scrape=True):
+
+    if scrape:
+        #Pull the latest data
+        print("Downloading the latest data:")
+        scrape_latest_data()
+        print()
+
+    #Get data files
+    data_dir = "./data/"
+    data_files = [data_dir + f for f in listdir(data_dir) 
+                  if isfile(join(data_dir, f)) 
+                  and f.endswith(".xlsx")]
+    
+    #Set list of target geographies
+    # NCL - E54000028, London - E40000003, England - E92000001
+    target_geographies = ["E54000028", "E40000003", "E92000001"]
+
+    #Split the files between Index and adult
+    print("Processing survival data:")
+    for data_file in data_files:
+        if data_file.split("/")[-1].startswith("Index"):
+            print(f"-> {data_file.split("/")[-1]}")
+            process_index_data(data_file, target_geographies)
+
+        if data_file.split("/")[-1].startswith("adult"):
+            print(f"-> {data_file.split("/")[-1]}")
+            process_adult_data_sheet4(data_file, target_geographies)
+
+
+main(scrape=False)
