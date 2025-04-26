510857b73d50bd16db122065e5f58a207ada5f7e
Jake Kealey <jake.kealey@nhs.net>
2025-04-26 21:02:46 +0100

    Created draft design for the adult 4 database table and create script and formatted the index create script.

--- docs/create_adult4.sql
+++ docs/create_adult4.sql
@@ -1,23 +1,21 @@
 --Create table statement for the Index data
-CREATE TABLE [Data_Lab_NCL_Dev].[GrahamR].[cancer_survival_index](
+CREATE TABLE [Data_Lab_NCL_Dev].[GrahamR].[cancer_survival_adult4](
 	area_code CHAR(9) NOT NULL,
 	area_name nVARCHAR(100) NOT NULL,
 	cancer_site nVARCHAR(40) NOT NULL,
 	gender nVARCHAR(7) NOT NULL,
-	age_at_diagnosis nVARCHAR(8) NOT NULL,
 	standardisation_type nVARCHAR(40) NOT NULL,
-	diagnosis_year SMALLINT NOT NULL,
+	standardisation_type_subcategory nVARCHAR(40),
 	years_since_diagnosis TINYINT NOT NULL,
 	patient_numbers INT NOT NULL,
+	survival_metric nVARCHAR(40) NOT NULL,
 	survival_per FLOAT NOT NULL,
-	lower_ci FLOAT,
-	upper_ci FLOAT,
-	precision FLOAT,
-	standard_error FLOAT,
-	data_substituted TINYINT NOT NULL,
+	data_diagnosis_window CHAR(9) NOT NULL,
+	data_snapshot nVARCHAR(14),
 	date_upload DATE NOT NULL,
 
 	PRIMARY KEY (area_code, cancer_site, 
-		gender, age_at_diagnosis, diagnosis_year, years_since_diagnosis)
+		gender, standardisation_type, years_since_diagnosis, 
+		survival_metric, data_diagnosis_window)
 );
 
