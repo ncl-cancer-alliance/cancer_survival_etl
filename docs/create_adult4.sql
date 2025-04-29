967bba9e3330f999d649fa192cc1ff6501c07501
Jake Kealey <jake.kealey@nhs.net>
2025-04-29 16:02:53 +0100

    Fixes to the CREATE TABLE scripts

--- docs/create_adult4.sql
+++ docs/create_adult4.sql
@@ -9,13 +9,13 @@ CREATE TABLE [Data_Lab_NCL_Dev].[GrahamR].[cancer_survival_adult4](
 	years_since_diagnosis TINYINT NOT NULL,
 	patient_numbers INT NOT NULL,
 	survival_metric nVARCHAR(40) NOT NULL,
-	survival_per FLOAT NOT NULL,
-	data_diagnosis_window CHAR(9) NOT NULL,
-	data_snapshot nVARCHAR(14),
+	survival_per FLOAT,
+	date_diagnosis_window CHAR(9) NOT NULL,
+	date_snapshot nVARCHAR(14),
 	date_upload DATE NOT NULL,
 
 	PRIMARY KEY (area_code, cancer_site, 
 		gender, standardisation_type, years_since_diagnosis, 
-		survival_metric, data_diagnosis_window)
+		survival_metric, date_diagnosis_window)
 );
 
